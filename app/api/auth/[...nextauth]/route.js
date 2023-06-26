ea456a8eb97ff3a4d0c42c8b517fae51140e1183
stublic.jurica <stublic.jurica@gmail.com>
2023-06-26 14:01:31 +0200

    removed DB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,7 +10,6 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  callbacks: {
       async session({ session }) {
     const sessionUser = await User.findOne({
         email: session.user.email,
@@ -42,7 +41,7 @@ const handler = NextAuth({
       return false;
     }
   },
-  }
+  
 
 });
 
